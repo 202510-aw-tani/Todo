8dc217496677f05d3e47e91ca1a5b6b87766ae91
TaMa <202510_aw_tani@x-ide.sakura.ne.jp>
2026-02-09 12:30:01 +0900

    Repositoryインターフェースの作成

---
+++ src/main/java/com/example/todo/repository/TodoRepository.java
@@ -0,0 +1,23 @@
+﻿package com.example.todo.repository;
+
+import com.example.todo.entity.Todo;
+import java.time.LocalDate;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface TodoRepository extends JpaRepository<Todo, Long> {
+
+    List<Todo> findByCompleted(Boolean completed);
+
+    List<Todo> findByTitleContaining(String keyword);
+
+    List<Todo> findByDueDateLessThanEqual(LocalDate date);
+
+    List<Todo> findAllByOrderByPriorityAsc();
+
+    // @Query usage example: same as findByTitleContaining + order by priority
+    @Query("select t from Todo t where t.title like %:keyword% order by t.priority asc")
+    List<Todo> searchByTitleOrderByPriority(@Param("keyword") String keyword);
+}
